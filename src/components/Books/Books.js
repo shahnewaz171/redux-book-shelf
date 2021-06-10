acbd3b21571bffdb358f633addb4669179f411c4
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-11 01:53:54 +0600

    update books section

--- src/components/Books/Books.js
+++ src/components/Books/Books.js
@@ -6,30 +6,30 @@ const Books = () => {
         <div className="container">
            <div className="row">
                 <div className="col-md-3 books">
-                    <ul class="list-group">
-                        <li class="list-group-item discover">Discover</li>
-                        <li class="list-group-item">
+                    <ul className="list-group">
+                        <li className="list-group-item discover">Discover</li>
+                        <li className="list-group-item">
                             Reading List
-                            <span class="badge bg-success ms-1">0</span>
+                            <span className="badge bg-success ms-1">0</span>
                         </li>
-                        <li class="list-group-item">
+                        <li className="list-group-item">
                             Finished Books
-                            <span class="badge bg-success ms-1">0</span>
+                            <span className="badge bg-success ms-1">0</span>
                         </li>
                     </ul>
                 </div>
 
                 <div className="col-md-9">
-                     <div class="card mb-3">
-                        <div class="row g-0">
-                            <div class="col-md-4">
+                     <div className="card mb-3">
+                        <div className="row g-0">
+                            <div className="col-md-4">
                                 <img src="..." alt="..." />
                             </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">Card title</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <div className="col-md-8">
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                    <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
                         </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,22 +5,22 @@ import './Navbar.css';
 const Navbar = () => {
     return (
         <div className="mb-4">
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container">
-                    <Link to="#" class="navbar-brand">Navbar</Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container">
+                    <Link to="#" className="navbar-brand">Navbar</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <Link to="#" class="nav-link active">Home</Link>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="#" className="nav-link active">Home</Link>
                             </li>
-                            <li class="nav-item">
-                                <Link to="#" class="nav-link">Link</Link>
+                            <li className="nav-item">
+                                <Link to="#" className="nav-link">Link</Link>
                             </li>
-                            <li class="nav-item">
-                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            <li className="nav-item">
+                                <button className="btn btn-outline-success" type="submit">Search</button>
                             </li>
                         </ul>
                     </div>
