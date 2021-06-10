309ed79609390380a4f0bdda06a3dcb4a627b99f
shahnewaz601 <shahnewaz601@gmail.com>
2021-06-11 01:35:22 +0600

    update navabr

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div>
-            
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container">
+                    <Link to="#" class="navbar-brand">Navbar</Link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link active">Home</Link>
+                            </li>
+                            <li class="nav-item">
+                                <Link to="#" class="nav-link">Link</Link>
+                            </li>
+                            <li class="nav-item">
+                                <button class="btn btn-outline-success" type="submit">Search</button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 };
