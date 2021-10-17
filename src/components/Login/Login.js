1da94a8f8ac3bf1bdb14539e71cc3324d271e361
shahnewaz601 <shahnewaz601@gmail.com>
2021-10-17 15:42:11 +0600

    designed login and signup form

---
+++ src/components/Login/Login.js
@@ -0,0 +1,73 @@
+import React, { useRef, useState } from 'react';
+import { useForm } from "react-hook-form";
+import { BsGoogle } from "react-icons/bs";
+
+const Login = () => {
+    const [logIn, setLogIn] = useState(true);
+    const { register, handleSubmit, watch, reset, formState: { errors } } = useForm({
+        mode: "all", 
+        reValidateMode: 'onChange'
+    });
+    const password = useRef();
+    password.current = watch('password');
+    
+    const onSubmit = data => console.log(data);
+
+    return (
+        <>
+            <h3 className="text-center text-primary">{logIn ? "Login Form" : "Signup Form"}</h3>
+            <div className="p-5">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    {logIn ?
+                        <>
+                            <div className="form-group mb-3">
+                                <input type="email" {...register("email", { required: true })} name="email" placeholder="Enter Email" className="form-control" />
+                                {errors.email && <span className="text-danger">This field is required</span>}
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="password" {...register("password", { required: true })} name="password" placeholder="Enter Password" className="form-control" />
+                                {errors.password && errors.password.type === "required" && <span className="text-danger">This field is required</span>}
+                            </div>
+                        </>
+                        :
+                        <>
+                            <div className="form-group mb-3">
+                                <input type="text" {...register("name", { required: true })} name="name" placeholder="Enter Name" className="form-control" />
+                                {errors.name && <span className="text-danger">This field is required</span>}
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="email" {...register("email", { required: true })} name="email" placeholder="Enter Email" className="form-control" />
+                                {errors.email && <span className="text-danger">This field is required</span>}
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="password" {...register("password", { required: true, minLength: 6 })} name="password" placeholder="Enter Password" className="form-control" />
+                                {errors.password && errors.password.type === "required" && <span className="text-danger">This field is required</span>}
+                                {errors.password && errors.password.type === "minLength" &&<span className="text-danger">Please enter at least 6 characters</span>}
+                            </div>
+                            <div className="form-group mb-3">
+                                <input type="password" {...register("c_password", { required: true, validate: (value) => value === password.current })} name="c_password" placeholder="Confirm Password" className="form-control" />
+                                {errors.c_password && errors.c_password.type === "required" && <span className="text-danger">This field is required</span>}
+                                {errors.c_password && errors.c_password.type === "validate" && <span className="text-danger">Password does not match</span>}
+                            </div>
+                        </>
+                    }
+                    <input type="submit" className="btn btn-primary btn-focus" value={logIn ? "Login" : "Register"} />
+                </form>
+                {logIn ?
+                   <p className="mt-2">
+                        Don't have an account? <span onClick={() => {reset(); setLogIn(!logIn)}} className="text-primary    text-decoration-underline cursor">Signup</span>
+                    </p>
+                    :
+                    <p className="mt-2">
+                        Already registered? <span onClick={() => {reset(); setLogIn(!logIn)}} className="text-primary text-decoration-underline cursor">Login</span>
+                    </p>
+                }
+                <div className="text-center">
+                    <button className="btn border border-1 border-primary rounded-pill text-black mt-3 btn-focus"><BsGoogle /> Continue with Google</button>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Login;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,34 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import { handleSignIn } from '../../utilities/auth';
+import Modal from 'react-modal';
 import './Navbar.css';
+import Login from '../Login/Login';
 
 const Navbar = () => {
-    console.log(process.env.REACT_APP_KEY)
-    console.log(process.env.REACT_APP_ID)
-    console.log(process.env.REACT_APP_SENDER_ID)
-    
+    const [modalIsOpen, setIsOpen] = useState(false);
+
+    const openModal = () => {
+        setIsOpen(true);
+    }
+
+    function closeModal() {
+        setIsOpen(false);
+    }
+
+
     return (
-        <div className="mb-4">
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <div className="container">
-                    <Link to="#" className="navbar-brand fw-bolder">Book<span className="text-primary">Self</span></Link>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <span className="nav-link active fw-bolder">Muhammad Shahnewaz</span>
-                            </li>
-                            <li className="nav-item">
-                               <button onClick={handleSignIn} className="btn btn-outline-primary">Login</button>
-                            </li>
-                        </ul>
+        <>
+            <div className="mb-4">
+                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <div className="container">
+                        <Link to="#" className="navbar-brand fw-bolder">Book<span className="text-primary">Self</span></Link>
+                        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <span className="nav-link active fw-bolder">Muhammad Shahnewaz</span>
+                                </li>
+                                <li className="nav-item r-out">
+                                    <button onClick={openModal} className="btn btn-primary btn-focus">Login</button>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
-            </nav>
-        </div>
+                </nav>
+            </div>
+
+            <Modal
+                isOpen={modalIsOpen}
+                onRequestClose={closeModal}
+                ariaHideApp={false}
+                contentLabel="Example Modal"
+            >
+                <Login />
+            </Modal>
+        </>
     );
 };
 
